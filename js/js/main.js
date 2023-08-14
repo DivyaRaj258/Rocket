01f496ae68216fd54b26db28b211df9e2ce0322b
DivyaRaj258 <divya.r258@gmail.com>
2023-08-14 16:09:31 +0200

    added astroids

--- js/js/main.js
+++ js/js/main.js
@@ -2,6 +2,11 @@ class Game {
     constructor() {
         this.player = new Player();
         this.obstaclesArr = []; //will store instances of the class Obstacle
+        this.astroidArr = []; //will store instances of the class Obstacle
+        
+        let score = 0;
+        
+          
     }
     start() {
 
@@ -12,17 +17,34 @@ class Game {
         setInterval(() => {
             const newObstacle = new Obstacle();
             this.obstaclesArr.push(newObstacle);
-        }, 3000);
+        }, 3500);
+
+         // create astroid
+         setInterval(() => {
+            const newAstroid = new Astroid();
+            this.astroidArr.push(newAstroid);
+        }, 2500);
 
         // move all obstacles
         setInterval(() => {
             this.obstaclesArr.forEach((obstacleInstance) => {
                 obstacleInstance.moveDown(); // move
                 this.removeObstacleIfOutside(obstacleInstance); // remove if outside
-                this.detectCollision(obstacleInstance); // detect collision
-
+                this.detectCollision(obstacleInstance);  // detect 100% collision
+             
             });
         }, 100);
+
+           // move astroid
+           setInterval(() => {
+            this.astroidArr.forEach((astroidInstance) => {
+                astroidInstance.moveDown(); // move
+                this.removeAstroidIfOutside(astroidInstance); // remove if outside
+                this.detectCollisionAstroid(astroidInstance); // detect collision
+
+            });
+        }, 50);
+        
     }
     attachEventListeners() {
         document.addEventListener("keydown", (event) => {
@@ -39,32 +61,51 @@ class Game {
             this.obstaclesArr.shift(); // remove from the array
         }
     }
+    // New method for Astroid
+    removeAstroidIfOutside(astroidInstance){
+        if (astroidInstance.positionY < 0 - astroidInstance.height) {
+            astroidInstance.domElement.remove(); //remove from the dom
+            this.astroidArr.shift(); // remove from the array
+        }
+    }
+    
     detectCollision(obstacleInstance)
     {
         if (
-            
-           /*
-            this.player.positionX === obstacleInstance.positionX + obstacleInstance.width &&
-            this.player.positionX + this.player.width > obstacleInstance.positionX &&
-            this.player.positionY === obstacleInstance.positionY + obstacleInstance.height &&
-            this.player.positionY + this.player.height > obstacleInstance.positionY
-            */
             
             // Modified Code
             this.player.positionX === obstacleInstance.positionX &&
             this.player.positionY === obstacleInstance.positionY &&
             this.player.width === obstacleInstance.width
             //this.player.height === obstacleInstance.height
-        
-                       
-        ) 
-         
-        {
+                 
+                
+         ) {
             // Collision detected! Change to Success Landing
-            console.log("The Eagle has Crashed ");
-            location.href = "gameover.html";
+          // Collision detected! Change to Success Landing
+            console.log("The Eagle has Landed ");
+            location.href = "gameoverWin.html";
+                     
         }
     }
+
+    detectCollisionAstroid(astroidInstance)
+    {
+        if (
+            
+           // Astroid Collision
+            this.player.positionX < astroidInstance.positionX + astroidInstance.width &&
+            this.player.positionX + this.player.width > astroidInstance.positionX &&
+            this.player.positionY < astroidInstance.positionY + astroidInstance.height &&
+            this.player.positionY + this.player.height > astroidInstance.positionY
+        ){
+                // Collision detected! Change to Success Landing
+                console.log("The Eagle has CRASHED ");
+                location.href = "gameoverCrash.html";
+        }
+          
+    }
+   
 }
 
 
@@ -154,6 +195,55 @@ class Obstacle {
     }
 }
 
+
+class Astroid {
+    constructor() {
+       
+        this.width = 10;
+        this.height = 20;
+        this.positionX = Math.floor(Math.random() * (100 - this.width + 1)); // random number between 0 and (100 - width)
+        this.positionY = 100;
+        this.domElement = null;
+        this.createDomElement();
+    }
+    createDomElement() {
+        // create dom element
+        this.domElement = document.createElement("div");
+
+            // Image Rocket code
+            this.domElement = document.createElement('img');
+            this.domElement.src = 'css/Astroid1.png'; 
+            this.domElement.style.width = this.width + 'px';
+            this.domElement.style.height = this.height + 'px';
+            this.domElement.style.position = 'absolute';
+            this.domElement.style.left = this.positionX + 'px';
+            //
+
+        // set id
+        this.domElement.className = "astroid";
+        this.domElement.style.width = this.width + "vw";
+        this.domElement.style.height = this.height + "vh";
+        this.domElement.style.left = this.positionX + "vw";
+        this.domElement.style.bottom = this.positionY + "vh";
+
+        //append to the dom
+        const parentElm = document.getElementById("board");
+        parentElm.appendChild(this.domElement);
+    }
+    moveDown() {
+        this.positionY -= 2;
+        this.domElement.style.bottom = this.positionY + "vh";
+    }
+
+    moveDiagonal() {
+        this.positionX -= 1;
+        this.positionY -= 2;
+        this.domElement.style.bottom = this.positionY + "vh";
+        this.domElement.style.bottom = this.positionX + "vh";
+    
+
+    }
+}
 
 const game = new Game();
 game.start();
